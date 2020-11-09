386ea61fd5a128a0594868ef6554ea6921099a13
Alex Sidorov <cobetckoeyxo@mail.ru>
2020-11-09 20:11:19 +1000

    5.7. Ипотека [#401489](the first correction)

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -4,11 +4,11 @@ public class Mortgage {
     public static int year(int amount, int salary, double percent) {
         int year = 0;
         percent = percent / 100;
-        double debt = amount * (percent + 1);
+        double debt = amount;
         while (debt > 0) {
             year++;
+            debt = debt * (percent + 1);
             debt -= salary;
-            debt += debt * percent;
         }
         return year;
     }
