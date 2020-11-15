2054d2429fa3ac0560fb2a07d169f58101b90c89
Alex Sidorov <cobetckoeyxo@mail.ru>
2020-11-15 12:38:26 +1000

    6.8. Дефрагментация массива [#401425](the first correct)

--- src/ru/job4j/array/Defragment.java
+++ src/ru/job4j/array/Defragment.java
@@ -6,14 +6,19 @@ public class Defragment {
             if (array[nullIndex] == null) {
                 for (int notNullIndex = nullIndex + 1; notNullIndex < array.length; notNullIndex++) {
                     if (array[notNullIndex] != null) {
-                        array[nullIndex] = array[notNullIndex];
-                        array[notNullIndex] = null;
+                        swap(array, nullIndex, notNullIndex);
                         break;
                     }
                 }
             }
             System.out.print(array[nullIndex] + " ");
         }
+        return array;
+    }
+
+    public static String[] swap(String[] array, int nullIndex, int notNullIndex){
+        array[nullIndex] = array[notNullIndex];
+        array[notNullIndex] = null;
         return array;
     }
 
