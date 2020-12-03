386517611d36d2df55c50a05b059a002d2a7f6d8
Alex Sidorov <cobetckoeyxo@mail.ru>
2020-12-03 23:00:45 +1000

    2. Scanner и чтение числа из консоли. [#401540](the first correction)

--- src/ru/job4j/io/Matches.java
+++ src/ru/job4j/io/Matches.java
@@ -5,31 +5,46 @@ import java.util.Scanner;
 public class Matches {
 
     public static void main(String[] args) {
+        String firstName, secondName;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Добро пожаловать в игру 11");
-        boolean run = true;
+        System.out.println("Добро пожаловать в игру 11!");
+        System.out.println("Введите имя первого игрока:");
+        firstName = scanner.nextLine();
+        System.out.println("Введите имя второго игрока:");
+        secondName = scanner.nextLine();
         int matchesCount = 11;
-        while (run) {
+        boolean firstPlayer = true;
+        while (true) {
+            if (firstPlayer) {
+                System.out.println("Ход игрока " + firstName);
+                firstPlayer = false;
+            } else {
+                System.out.println("Ход игрока " + secondName);
+                firstPlayer = true;
+            }
             System.out.println("Тяните спички от 1 до 3");
             int matchesLeft = Integer.valueOf(scanner.nextLine());
             if (matchesLeft < 1 || matchesLeft > 3) {
-                System.out.println("Нельзя столько брать! Тяните снова.");
+                System.out.println("Нельзя столько брать! Повторите попытку.");
+                firstPlayer = !firstPlayer;
                 continue;
             }
             matchesCount -= matchesLeft;
-            printMatches(matchesCount);
             if (matchesCount <= 0) {
-                run = false;
+                System.out.println("Спичек не осталось!");
+                if (!firstPlayer) {
+                    System.out.println("Победил игрок " + firstName + "!");
+                } else {
+                    System.out.println("Победил игрок " + secondName + "!");
+                }
+                break;
             }
+            printMatches(matchesCount);
         }
         scanner.close();
     }
 
     private static void printMatches(int match) {
-        if (match <= 0) {
-            System.out.println("Спичек не осталось, вы выиграли!");
-        } else {
-            System.out.println("Спичек осталось " + match);
-        }
+        System.out.println("Спичек осталось " + match);
     }
 }
