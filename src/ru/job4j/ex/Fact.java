4456ad03545413ad18ce904341025d4090e1520e
Sidorov Aleksey <cobetckoeyxo@mail.ru>
2020-12-20 23:49:19 +1000

    0.2. Кидаем исключение - throw new RuntimeException [#401559]

---
+++ src/ru/job4j/ex/Fact.java
@@ -0,0 +1,20 @@
+package ru.job4j.ex;
+
+public class Fact {
+    public static void main(String[] args) {
+        System.out.println(new Fact().calc(9));
+        System.out.println(new Fact().calc(-2));
+    }
+
+    public int calc(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Wrong number n. n should be more then 0 or equals");
+        } else {
+            int rsl = 1;
+            for (int index = 1; index <= n; index++) {
+                rsl *= index;
+            }
+            return rsl;
+        }
+    }
+}
