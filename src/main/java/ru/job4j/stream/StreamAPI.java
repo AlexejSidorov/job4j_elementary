a738523615a7d0cfba01a54a441e4d35dc34c74e
Sidorov Aleksey <cobetckoeyxo@mail.ru>
2021-03-16 02:37:19 +1000

    0. Stream API [#401350]

---
+++ src/main/java/ru/job4j/stream/StreamAPI.java
@@ -0,0 +1,15 @@
+package ru.job4j.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamAPI {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, -2, 3, -4, 5, -6, 7, -8, 9, -10);
+        List<Integer> positiveList = list.stream()
+                .filter(e -> e >= 0)
+                .collect(Collectors.toList());
+        positiveList.forEach(System.out::println);
+    }
+}
