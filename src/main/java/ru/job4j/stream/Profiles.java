aab0a7691c415af0d2ae85bceddd6cd92638d9fe
Sidorov Aleksey <cobetckoeyxo@mail.ru>
2021-03-17 02:22:58 +1000

    3. Уникальность элементов и сортировка. [#401353]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,5 +1,6 @@
 package ru.job4j.stream;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,6 +9,8 @@ public class Profiles {
     public List<Address> collect(List<Profile> profiles) {
         List<Address> list = profiles.stream()
                 .map(p -> p.getAddress())
+                .sorted((a1, a2) -> a1.getCity().compareTo(a2.getCity()))
+                .distinct()
                 .collect(Collectors.toList());
         return list;
     }
--- test/ru/job4j/stream/ProfilesTest.java
+++ test/ru/job4j/stream/ProfilesTest.java
@@ -4,45 +4,25 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-
-import static org.junit.Assert.*;
 
 public class ProfilesTest {
 
     @Test
     public void ifProfileGetAddressSuccess() {
-        List<Address> expectAddresses = Arrays.asList(
-                new Address("1", "1", 1, 1),
-                new Address("2", "2", 2, 2),
-                new Address("3", "3", 3, 3),
-                new Address("4", "4", 4, 4)
-        );
+        List<Address> expectAddresses = new ArrayList<>();
+        expectAddresses.add(new Address("1", "1", 1, 1));
+        expectAddresses.add(new Address("4", "4", 4, 4));
+        expectAddresses.add(new Address("3", "3", 3, 3));
+        expectAddresses.add(new Address("4", "4", 4, 4));
+
         List<Profile> profileList = new ArrayList<>();
         for (Address address : expectAddresses) {
             profileList.add(new Profile(address));
         }
         Profiles profiles = new Profiles();
         List<Address> actualAddresses = profiles.collect(profileList);
+        expectAddresses.remove(1);
         Assert.assertEquals(expectAddresses, actualAddresses);
-    }
-
-    @Test
-    public void ifProfileGetAddressFail() {
-        List<Address> expectAddresses = new ArrayList<>();
-        expectAddresses.add(new Address("1", "1", 1, 1));
-        expectAddresses.add(new Address("2", "2", 2, 2));
-        expectAddresses.add(new Address("3", "3", 3, 3));
-        expectAddresses.add(new Address("4", "4", 4, 4));
-
-        List<Profile> profileList = expectAddresses.stream()
-                .map(Profile::new).collect(Collectors.toList());
-        Profiles profiles = new Profiles();
-        expectAddresses.remove(0);
-        expectAddresses.add(new Address("5", "5", 5, 5));
-        List<Address> actualAddresses = profiles.collect(profileList);
-        Assert.assertNotEquals(expectAddresses, actualAddresses);
     }
 }
