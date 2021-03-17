03164fc93f33892ab7da79b446b3806f252ecfc7
Sidorov Aleksey <cobetckoeyxo@mail.ru>
2021-03-18 02:32:00 +1000

    2. Список адресов [#401352](return corr)

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,17 +1,13 @@
 package ru.job4j.stream;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
 
     public List<Address> collect(List<Profile> profiles) {
-        List<Address> list = profiles.stream()
-                .map(p -> p.getAddress())
-                .sorted((a1, a2) -> a1.getCity().compareTo(a2.getCity()))
-                .distinct()
+        return profiles.stream()
+                .map(Profile::getAddress)
                 .collect(Collectors.toList());
-        return list;
     }
 }
